4c1c2c065732a9593c11ad8e8fb35f40266f5251
Miguel Medina Ballesteros <maximetinu@gmail.com>
2019-08-09 10:22:33 +0200

    Water Visuals
    Just followed these tutorials:
    https://lindenreid.wordpress.com/2017/12/18/unity-graphics-setup/
    for project graphic settings
    
    and
    https://www.patreon.com/posts/making-water-24192529
    for water shader and ripple effect

---
+++ Assets/Water Visuals/Scripts/GenerateDepthTexture.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace WaterBuoyancy
+{
+    [RequireComponent(typeof(Camera))]
+    public class GenerateDepthTexture : MonoBehaviour
+    {
+        public DepthTextureMode mode;
+        private void OnValidate() => GetComponent<Camera>().depthTextureMode = mode;
+    }
+}
+
